539e4e626baafa419383a6be45dc60babb3732a6
yash2044jaiswal <yash2044jaiswal@gmail.com>
2026-08-25 12:03:52 +0530

    Update AuthContext.jsx

--- Client/src/context/AuthContext.jsx
+++ Client/src/context/AuthContext.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useState, useContext, useEffect } from 'react'
-import { authService } from '../Services/authService'
+import { authService } from '../Services/authService.js'
 import toast from 'react-hot-toast'
 
 const AuthContext = createContext()
